8ca4b82b04ec81d0a1d23a8f0d102d9be1d79ad1
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-04-16 15:24:46 +0100

    Update Framework.lua

--- Trident/Framework.lua
+++ Trident/Framework.lua
@@ -12,7 +12,7 @@ local Lighting = game:GetService("Lighting")
 local Framework = {Settings={FullBright=true,Fov=90}}
 local Esp = {Settings={Boxes=false,Distances=false,Armor=false,ItemDistances=false,ItemNames=false,OreDistances=false,OreNames=false,PlayerRenderDistance=1000,ItemRenderDistance=1000,OreRenderDistance=1000,PlayerBoxColor=Color3.fromRGB(120,81,169),PlayerDistanceColor=Color3.fromRGB(120,81,169),PlayerArmorColor=Color3.fromRGB(120,81,169),Sleeping=false,PlayerSleepingColor=Color3.fromRGB(120,81,169),LocalChamsColor=Color3.fromRGB(120,81,169),LocalChamsMaterial=Enum.Material.ForceField},Drawings={},Connections={}}
 local Crosshair = {Enabled=false,CrosshairThickness=2,CrosshairSize=8,CrosshairColor=Color3.fromRGB(255,0,255),X,Y}
-local Aimbot = {Settings={FovEnabled=false,FovTransparency=1,FovSize=90,FovFilled=false,FovColor=Color3.fromRGB(120,81,169)},Fov={},FovCircleDrawing=nil,AimbotHitpart="Head",AimbotSmoothing=3,Prediction=false,DropPrediction=false,TargetSleepers=false}
+local Aimbot = {Settings={FovEnabled=false,FovTransparency=1,FovSize=90,FovFilled=false,FovColor=Color3.fromRGB(120,81,169),TargetSleepers=false},Fov={},FovCircleDrawing=nil,AimbotHitpart="Head",AimbotSmoothing=3,Prediction=false,DropPrediction=false}
 local AllowedOres = {"StoneOre","NitrateOre","IronOre"}
 local AllowedItems = {"PartsBox","MilitaryCrate","SnallBox","SnallBox","Backpack","VendingMachine"}
 
