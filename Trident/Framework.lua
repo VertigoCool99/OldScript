b3d759a7e11ad46cb33e26fe66d382bbcdaf5866
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-04-17 13:02:59 +0100

    Update Framework.lua

--- Trident/Framework.lua
+++ Trident/Framework.lua
@@ -312,7 +312,7 @@ function Aimbot:InFov(Model)
     if not Model then return false end
     local playerpos = Camera:WorldToViewportPoint(Model:GetPivot().p)
     local distance = (Vector2.new(playerpos.X,playerpos.Y) - Vector2.new(Mouse.X,Mouse.Y)).Magnitude
-    if distance <= Aimbot.FovCircleDrawing.Radius + 20 then
+    if distance <= Aimbot.FovCircleDrawing.Radius + 25 then
         return true
     end
     return false
