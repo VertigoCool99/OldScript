91f67b91a2a9a7a759c0cae3cb6515bbaff6d638
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2025-01-26 13:32:22 +0000

    Update PathfindCarry.lua

--- Dungeon Quest/PathfindCarry.lua
+++ Dungeon Quest/PathfindCarry.lua
@@ -4,7 +4,7 @@ getgenv().SkillWait = 0
 --Locals
 local Workspace,PathfindingService,Players = game:GetService("Workspace"),game:GetService("PathfindingService"),game:GetService("Players")
 local Path = PathfindingService:CreatePath({AgentRadius = 3,WaypointSpacing=7,AgentHeight = 6,AgentCanJump = false,Costs = {Neon = 1}})
-local waypoints,nextWaypointIndex,reachedConnection,blockedConnection,Speed = {},1,nil,nil,60
+local waypoints,nextWaypointIndex,reachedConnection,blockedConnection,Speed = {},1,nil,nil,55
 
 function TweenPlayer(destination)
     local distance = (destination - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
